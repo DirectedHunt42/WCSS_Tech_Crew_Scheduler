149bf9139ea1bd1d7c1d5713d381dcfedad007d3
DirectedHunt42 <jackpwmurray@gmail.com>
2025-06-05 12:05:43 -0400

    Testing

--- JS/calendar_script.js
+++ JS/calendar_script.js
@@ -164,7 +164,7 @@ calendarDates.addEventListener('click', async (event) => {
     console.log('Selected date:', backendDate);
 
     // Fetch events for the selected date from the new API
-    const response = await fetch('/api/events-by-date?date=' + backendDate, { credentials: 'include' });
+    const response = await fetch('http://10.191.28.44:5500/api/events-by-date?date=' + backendDate, { credentials: 'include' });
     if (!response.ok) {
         alert('Failed to fetch events for this date.');
         return;
