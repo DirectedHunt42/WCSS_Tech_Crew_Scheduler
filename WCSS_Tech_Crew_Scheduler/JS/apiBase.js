51a595779e70febbb3c26723e6a191db8546ac01
DirectedHunt42 <jackpwmurray@gmail.com>
2025-06-13 12:26:47 -0400

    Removed duplicate folder

--- WCSS_Tech_Crew_Scheduler/JS/apiBase.js
+++
@@ -1,5 +0,0 @@
-// Central API base URL for all frontend scripts
-
-// Change this to your backend's base URL as needed
-export const apiBase = window.location.protocol + "//" + window.location.hostname + ":5500";
-export const apiBase2 = window.location.protocol + "//" + window.location.hostname;
